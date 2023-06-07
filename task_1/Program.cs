ea692db99fbf7b5b99457ef334893b894ba8cbf5
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-06-07 21:17:09 +0300

    completed 2 tasks

---
+++ task_1/Program.cs
@@ -0,0 +1,51 @@
+﻿string IfNumberIsNegative(int[] array)
+{
+    string result = string.Empty;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) count++;
+        else continue;
+    }
+    return result + count;
+}
+
+string PrintArray(int[] array)
+{
+    string result = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    return result;
+}
+
+
+
+
+try
+{
+    Console.Write("Insert the quantity of numbers that your array should contain = ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 1)
+    {
+        int[] mainarray = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            Console.Write("Insert the number = ");
+            mainarray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.Clear();
+        Console.Write("Your array: ");
+        PrintArray(mainarray);
+        Console.WriteLine();
+        Console.Write("The quantity of negative numbers in the array = ");
+        Console.WriteLine(IfNumberIsNegative(mainarray));
+    }
+    else if (num == 0 || num == 1) Console.WriteLine("You may not insert this number - try another one");
+    else Console.WriteLine("You may insert only positive numbers");
+}
+catch
+{
+    Console.WriteLine("You may insert only numbers");
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿string Coord(double k, double b, double m, double l)
+{
+    string result = String.Empty;
+    double x = 0;
+    double y = 0;
+    x = (l - b) / (k - m);
+    y = k * x + b;
+    return result + $"x = {x}, y = {y}";
+}
+
+
+
+
+try
+{
+    Console.WriteLine("Insert the necessary coefficients of the equation y = kx + b");
+    Console.Write("k = ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("b = ");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+    Console.Clear();
+    Console.WriteLine("Insert the necessary coefficients of the equation y = mx + l");
+    Console.Write("m = ");
+    double num3 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("l = ");
+    double num4 = Convert.ToDouble(Console.ReadLine());
+    Console.Clear();
+    if (num1 != num3)
+    {
+        Console.WriteLine(Coord(num1, num2, num3, num4));
+    }
+    else Console.WriteLine("These are parallel lines");
+}
+catch
+{
+    Console.WriteLine("You may insert only numbers");
+}
